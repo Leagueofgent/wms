a9a77018cbe1b098eb8c36271ce276f9ad77154e
guoyang <13560104516@139.com>
2024-01-14 19:44:38 +0800

    完善security框架自定义登录

---
+++ src/main/java/com/warehouse/management/wms/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,51 @@
+package com.warehouse.management.wms.filter;
+
+import com.warehouse.management.wms.config.RedisCache;
+import com.warehouse.management.wms.entity.LoginUser;
+import com.warehouse.management.wms.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Resource
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取token
+        String token = request.getHeader("token");
+        if (!StringUtils.hasText(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        //解析token
+        Claims claims = JwtUtil.parseJwt(token);
+        String subject = claims.getSubject();
+        //从redis中获取用户信息
+        String redisKey = "login" + subject;
+        LoginUser loginUser = redisCache.getCacheObject(redisKey);
+        if (Objects.isNull(loginUser)) {
+            throw new RuntimeException("用户未登录");
+        }
+        //存入SecurityContextHolder
+        // TODO 获取权限信息的封装到Authentication中
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, null);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}
